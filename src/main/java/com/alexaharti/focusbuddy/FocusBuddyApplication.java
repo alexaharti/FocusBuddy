33acf465945304ef817bed203c1557e688c04be5
Harti Alexandrina <hartialexandrina@gmail.com>
2026-08-19 15:53:59 +0300

    Initialize backend and Docker environment

---
+++ src/main/java/com/alexaharti/focusbuddy/FocusBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.alexaharti.focusbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexaharti/focusbuddy/FocusBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexaharti.focusbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
